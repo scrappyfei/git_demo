c93df5aa7acea7055514b89cac454649dcd243af
Fei <51778467@qq.com>
2023-04-18 10:01:17 +0800

    add child_two create Demo01.java

---
+++ child_two/src/main/java/Demo01.java
@@ -0,0 +1,8 @@
+public class Demo01 {
+    public static void main(String[] args) {
+        GitHub_Push gitHubPush = new GitHub_Push();
+        gitHubPush.main(args);
+        System.out.println("Hello World");
+
+    }
+}
