2eefd8f7589ee2261d9b8d24e07a7d063eb82269
Fei <51778467@qq.com>
2023-04-18 10:58:44 +0800

    test conflict

--- child_one/src/main/java/GitHub_Push.java
+++ child_one/src/main/java/GitHub_Push.java
@@ -5,5 +5,7 @@ public class GitHub_Push {
     public static void main(String[] args) {
         System.out.println("Hello World");
         log.info("dev branch");
+        System.out.println("dev branch");// dev branch
+        System.out.println("conflicts");// conflict
     }
 }
