da5eb44e8170381e1aae066e63708d72049c7fba
Fei <51778467@qq.com>
2023-04-18 10:24:06 +0800

    test new branch dev

---
+++ child_one/src/main/java/Dev_One.java
@@ -0,0 +1,8 @@
+public class Dev_One extends GitHub_Push {
+    private String name = "Dev_One";
+    private String password = "123456";
+
+    public void show() {
+        System.out.println("name: " + name + " password: " + password);
+    }
+}
--- child_one/src/main/java/GitHub_Push.java
+++ child_one/src/main/java/GitHub_Push.java
@@ -1,5 +1,9 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class GitHub_Push {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        log.info("dev branch");
     }
 }
